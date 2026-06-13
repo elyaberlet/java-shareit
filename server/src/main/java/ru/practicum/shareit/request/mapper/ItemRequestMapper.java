f7c1473173c2b5f9e0a3e77346d8e7495729f114
unknown <eleonora.brueva@yandex.com>
2026-06-13 15:34:59 +0300

    Убрать ненужный импорт

--- server/src/main/java/ru/practicum/shareit/request/mapper/ItemRequestMapper.java
+++ server/src/main/java/ru/practicum/shareit/request/mapper/ItemRequestMapper.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.request.mapper;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.request.ItemRequest;
